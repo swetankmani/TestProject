e3b3946b1b1135f6c88f6b538704d4e394cb6250
ASK <ASK@ASK-PC>
2022-06-28 16:48:21 +0530

    Exception handing concepts

---
+++ TestProject/src/ExceptionHandlingDemo.java
@@ -0,0 +1,30 @@
+
+public class ExceptionHandlingDemo {
+	
+	public static void main(String[] args) {
+		
+		try {
+			
+			System.out.println("Hello World");
+			
+			int i= 1/0;
+			
+			System.out.println("Completed");
+		} catch(Exception exp) {
+			
+			System.out.println("I am inside catch block");
+			System.out.println("Message is : " + exp.getMessage());
+			System.out.println("Cause is : " + exp.getCause());
+			exp.printStackTrace();
+			
+		}
+		finally {
+			
+			System.out.println("I am inside finally");
+			
+		}
+		
+		
+	}
+
+}
